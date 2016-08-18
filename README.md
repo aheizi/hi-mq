4bb67cc67b1062a0aa714f098d5dbd96a9149d9f
noly <aheizi@outlook.com>
2016-08-19 00:20:25 +0800

    Publish/Subscribe && Routing

---
+++ README.md
@@ -0,0 +1,7 @@
+###翻译RabbitMQ官方Java教程
+1. [RabbitMQ之HelloWorld](http://www.cnblogs.com/aheizi/p/5755021.html)  
+2. [RabbitMQ之任务队列](http://www.cnblogs.com/aheizi/p/5778369.html)  
+3. [RabbitMQ之发布订阅](http://www.cnblogs.com/aheizi/p/5782469.html) 
+4. [RabbitMQ之路由](http://www.cnblogs.com/aheizi/p/5785992.html)  
+
+[RabbitMQ官网](https://www.rabbitmq.com)
---
+++ src/main/java/publish/ReceiveLogs.java
@@ -0,0 +1,35 @@
+package publish;
+
+import com.rabbitmq.client.*;
+
+import java.io.IOException;
+
+/**
+ * Created by noly on 16/8/19.
+ */
+public class ReceiveLogs {
+    private static final String EXCHANGE_NAME = "logs";
+
+    public static void main(String[] argv) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+
+        channel.exchangeDeclare(EXCHANGE_NAME, "fanout");
+        String queueName = channel.queueDeclare().getQueue();
+        channel.queueBind(queueName, EXCHANGE_NAME, "");
+
+        System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+
+        Consumer consumer = new DefaultConsumer(channel) {
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope,
+                                       AMQP.BasicProperties properties, byte[] body) throws IOException {
+                String message = new String(body, "UTF-8");
+                System.out.println(" [x] Received '" + message + "'");
+            }
+        };
+        channel.basicConsume(queueName, true, consumer);
+    }
+}
