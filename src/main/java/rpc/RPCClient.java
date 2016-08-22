d913afe90e8214bb2994f63b14577dd12ffb3e65
noly <aheizi@outlook.com>
2016-08-22 23:33:01 +0800

    add rpc

---
+++ src/main/java/rpc/RPCClient.java
@@ -0,0 +1,77 @@
+package rpc;
+
+import com.rabbitmq.client.*;
+
+import java.util.UUID;
+
+/**
+ * Created by noly on 16/8/22.
+ */
+public class RPCClient {
+    private Connection connection;
+    private Channel channel;
+    private String requestQueueName = "rpc_queue";
+    private String replyQueueName;
+    private QueueingConsumer consumer;
+
+    public RPCClient() throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        connection = factory.newConnection();
+        channel = connection.createChannel();
+
+        replyQueueName = channel.queueDeclare().getQueue();
+        consumer = new QueueingConsumer(channel);
+        channel.basicConsume(replyQueueName, true, consumer);
+    }
+
+    public String call(String message) throws Exception {
+        String response = null;
+        String corrId = UUID.randomUUID().toString();
+
+        AMQP.BasicProperties props = new AMQP.BasicProperties
+                .Builder()
+                .correlationId(corrId)
+                .replyTo(replyQueueName)
+                .build();
+
+        channel.basicPublish("", requestQueueName, props, message.getBytes("UTF-8"));
+
+        while (true) {
+            QueueingConsumer.Delivery delivery = consumer.nextDelivery();
+            if (delivery.getProperties().getCorrelationId().equals(corrId)) {
+                response = new String(delivery.getBody(),"UTF-8");
+                break;
+            }
+        }
+
+        return response;
+    }
+
+    public void close() throws Exception {
+        connection.close();
+    }
+
+    public static void main(String[] argv) {
+        RPCClient fibonacciRpc = null;
+        String response = null;
+        try {
+            fibonacciRpc = new RPCClient();
+
+            System.out.println(" [x] Requesting fib(30)");
+            response = fibonacciRpc.call("30");
+            System.out.println(" [.] Got '" + response + "'");
+        }
+        catch  (Exception e) {
+            e.printStackTrace();
+        }
+        finally {
+            if (fibonacciRpc!= null) {
+                try {
+                    fibonacciRpc.close();
+                }
+                catch (Exception ignore) {}
+            }
+        }
+    }
+}
